6de7cbea59a2412805a135ef1220173bf45378ea
Brent <bmiller180787@gmail.com>
2023-04-27 11:22:55 +0100

    adding state to common parent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./Components/Header";
 import Content from "./Components/Content";
 import Footer from "./Components/Footer";
+import Button from "./Components/Footer/Button";
+import {useState} from "react";
 
 const App = () => {
+    const [loggedIn, setLogged In] = useState(false)
+
   return (
       <>
         <Header />
